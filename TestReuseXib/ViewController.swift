a98b3f129acdb889a4e0b031b358be0698738da5
devin.wang <devin.wang@skyscanner.net>
2016-04-19 13:52:02 +0800

    Initial Commit

---
+++ TestReuseXib/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestReuseXib
+//
+//  Created by Devin Wang on 16/4/19.
+//  Copyright © 2016年 devin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
